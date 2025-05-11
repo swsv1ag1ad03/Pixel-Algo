c1a49dda099150eb3b9e5e33d79923b9c5abd1d9
swsv1ag1ad03 <167692144+swsv1ag1ad03@users.noreply.github.com>
2025-05-11 19:51:35 +0500

    Update index1.1. script.js

--- index1.1. script.js
+++ index1.1. script.js
@@ -1,1 +1,13 @@
+let knopka = document.querySelectorAll("button");
+let cartAmount = document.querySelector('.cart-amount');
+cartAmount.innerHTML = "0";
+function knopka1() {
+    cartAmount.innerHTML = +cartAmount.innerHTML + 1;
+    console.log(cartAmount.innerHTML)
+}
+
+console.log(knopka)
+for (let i = 0; i < knopka.length; i += 1) {
+    knopka[i].addEventListener('click', knopka1);
+}
 
